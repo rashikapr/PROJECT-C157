2c78df32acada3766f3688aa4c689edea8f5dc56
rashikapr <74289013+rashikapr@users.noreply.github.com>
2024-02-27 11:44:03 +0530

    Update Posters.js

--- js/Posters.js
+++ js/Posters.js
@@ -6,23 +6,7 @@ AFRAME.registerComponent("comics-posters", {
 
   posters: function() {
     const postersRef = [
-      {
-        id: "superman",
-        url: "./assets/posters/superman-poster.jpg"
-      },
-      {
-        id: "spiderman",
-        url: "./assets/posters/spiderman-poster.jpg"
-      },
-
-      {
-        id: "captain-aero",
-        url: "./assets/posters/captain-aero-poster.jpg"
-      },
-      {
-        id: "outer-space",
-        url: "./assets/posters/outer-space-poster.jpg"
-      }
+      
     ];
     let prevoiusXPosition = -60;
 
@@ -44,29 +28,10 @@ AFRAME.registerComponent("comics-posters", {
     }
   },
   createBorder: function(position, id) {
-    const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("id", id);
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("geometry", {
-      primitive: "plane",
-      width: 22,
-      height: 40
-    });
-
-    entityEl.setAttribute("position", position);
-    entityEl.setAttribute("material", { color: "#fff" });
-
-    return entityEl;
+     
   },
   createPoster: function(item) {
-    const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("geometry", {
-      primitive: "plane",
-      width: 20,
-      height: 28
-    });
-
+    
     entityEl.setAttribute("position", { x: 0, y: 5, z: 0.1 });
     entityEl.setAttribute("material", { src: item.url });
 
